2a96f28ba5d26ecac0aac800694c026b84741d1a
Dhairya Rana <cloudofdhairya@gmail.com>
2025-02-06 23:46:50 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const favButtons = document.querySelectorAll(".fav-btn");
+    favButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            button.innerText = "Added to Faves ❤️";
+            button.style.background = "#4CAF50";
+        });
+    });
+ });
